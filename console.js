2bd9f6a7378d40c848fb183f5b91db1ccab5b52b
Adeeyo Michael <118169702+Ademic2022@users.noreply.github.com>
2023-06-17 10:59:31 -0700

    Update console.js

--- console.js
+++ console.js
@@ -1,1 +1,2 @@
-console.log("this is my first javascript file");
+console.log("this is my first javascript file"); //first script
+console.log("mastering console object")  //second script with console
